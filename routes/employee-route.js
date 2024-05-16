428bbe722c954318542896e6d8209916f6f48393
lalankeba <lalankeba@gmail.com>
2024-05-16 15:59:20 +0530

    adding swagger doc to get employees

--- routes/employee-route.js
+++ routes/employee-route.js
@@ -5,7 +5,44 @@ const checkRoles = require('../middleware/check-roles');
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /employees:
+ *   get:
+ *     tags:
+ *       - Employees
+ *     summary: Returns a list of employees.
+ *     description: Admins can retrieve list of employees
+ *     parameters:
+ *       - name: Authorization
+ *         in: header
+ *         description: Bearer token for authorization.
+ *         required: true
+ *         schema:
+ *           type: string
+ *       - name: page, 
+ *         in: query, 
+ *         description: Page number. Starting from 0,
+ *         required: false
+ *         schema:
+ *           type: integer
+ *       - name: size, 
+ *         in: query, 
+ *         description: Number of employees to be retrieved. Max is 100,
+ *         required: false
+ *         schema:
+ *           type: integer
+ *     responses: 
+ *       200:
+ *         description: List of employees
+ *       401:
+ *         description: No permissions to retrieve list of employees
+ *       500:
+ *         description: Invalid input
+ *         
+ */
 router.get('/', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), getEmployees);
+
 router.get('/employee', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN', 'EMPLOYEE']), getEmployee);
 router.get('/employee/:id', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), getOtherEmployee);
 router.put('/', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN', 'EMPLOYEE']), updateEmployee);
