78e04d8f6e83b3609c6152b8d0a302652921643f
lalankeba <lalankeba@gmail.com>
2024-05-16 21:57:15 +0530

    updating swagger docs for employee

--- routes/employee-route.js
+++ routes/employee-route.js
@@ -39,10 +39,105 @@ const router = express.Router();
  */
 router.get('/', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), getEmployees);
 
+/**
+ * @swagger
+ * /employees/employee:
+ *   get:
+ *     tags:
+ *       - Employees
+ *     summary: Returns the logged in employee.
+ *     description: Returns the currently logged in employee.
+ *     security:
+ *       - bearerAuth: []
+ *     responses: 
+ *       200:
+ *         description: Logged in employee
+ *       500:
+ *         description: Invalid input
+ *         
+ */
 router.get('/employee', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN', 'EMPLOYEE']), getEmployee);
+
+/**
+ * @swagger
+ * /employees/employee/{id}:
+ *   get:
+ *     tags:
+ *       - Employees
+ *     summary: Returns an employee by id
+ *     description: Admins can view an employee by id
+ *     security:
+ *       - bearerAuth: []
+ *     responses: 
+ *       200:
+ *         description: Details of an employee
+ *       404:
+ *         description: No employee for the id
+ *       500:
+ *         description: Invalid input
+ *         
+ */
 router.get('/employee/:id', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), getOtherEmployee);
+
+/**
+ * @swagger
+ * /employees/employee/{id}:
+ *   put:
+ *     tags:
+ *       - Employees
+ *     summary: Updates logged in employee
+ *     description: Any employee can update his/her own information
+ *     security:
+ *       - bearerAuth: []
+ *     responses: 
+ *       200:
+ *         description: Updated details of the logged in employee
+ *       500:
+ *         description: Invalid input
+ *         
+ */
 router.put('/', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN', 'EMPLOYEE']), updateEmployee);
-router.put('/', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), updateOtherEmployee);
+
+/**
+ * @swagger
+ * /employees/{id}:
+ *   put:
+ *     tags:
+ *       - Employees
+ *     summary: Updates an employee by id
+ *     description: Admins can update an employee by id
+ *     security:
+ *       - bearerAuth: []
+ *     responses: 
+ *       200:
+ *         description: Updated details of an employee
+ *       404:
+ *         description: No employee for the id
+ *       500:
+ *         description: Invalid input
+ *         
+ */
+router.put('/:id', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), updateOtherEmployee);
+
+/**
+ * @swagger
+ * /employees/{id}:
+ *   delete:
+ *     tags:
+ *       - Employees
+ *     summary: Deletes an employee by id
+ *     description: Admins can delete an employee by id
+ *     security:
+ *       - bearerAuth: []
+ *     responses: 
+ *       200:
+ *         description: Deleted details of an employee
+ *       404:
+ *         description: No employee for the id
+ *       500:
+ *         description: Invalid input
+ *         
+ */
 router.delete('/:id', passport.authenticate('jwt', { session: false }), checkRoles(['ADMIN']), deleteEmployee);
 
 module.exports = router;
