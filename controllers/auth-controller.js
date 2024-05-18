d728a82f9442bf3075576450746785d3b7218921
lalankeba <lalankeba@gmail.com>
2024-05-18 10:02:03 +0530

    updating login logic in service layer

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,7 +1,3 @@
-const employeeModel = require('../models/employee-model');
-const bcrypt = require('bcryptjs');
-const jsonwebtoken = require('jsonwebtoken');
-const { v4: uuidv4 } = require('uuid');
 const validatePassword = require('../utils/password-validator-util');
 const authService = require('../services/auth-service');
 const InvalidArgsError = require('../errors/invalid-args-error');
@@ -28,26 +24,16 @@ const login = async (req, res) => {
     try {
         const { username, password } = req.body;
         if (!username || !password) {
-            throw new Error('Credentials are required');
+            throw new InvalidArgsError('Credentials are required');
         }
-        const employee = await employeeModel.findOne({ username: username });
-        if (!employee) { // no employee found for the provided username
-            res.status(400).json({ message: 'Credentials are invalid' });
-        } else {
-            const isMatch = await bcrypt.compare(password, employee.password);
-            if (!isMatch) { // password does not match
-                res.status(400).json({ message: 'Credentials are invalid' });
-            } else { // valid employee
-                const token = jsonwebtoken.sign(
-                    { jwtid: uuidv4(), username: employee.username, roles: employee.roles }, 
-                    process.env.JWT_SECRET,
-                    { algorithm: 'HS512', expiresIn: '1d' }
-                );
-                res.status(200).json({ token });
-            }
-        }
+        const token = await authService.login(username, password);
+        res.status(200).json({ token });
     } catch (err) {
-        res.status(500).json({ message: err.message });
+        if (err instanceof InvalidArgsError) {
+            res.status(400).json({ message: err.message });
+        } else {
+            res.status(500).json({ message: err.message });
+        }
     }
 }
 
--- services/auth-service.js
+++ services/auth-service.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs');
 const employeeModel = require('../models/employee-model');
 const InvalidArgsError = require('../errors/invalid-args-error');
+const jsonwebtoken = require('jsonwebtoken');
+const { v4: uuidv4 } = require('uuid');
 
 const register = async (firstName, lastName, gender, username, password) => {
     const oldEmployee = await employeeModel.findOne({username: username});
@@ -12,4 +14,23 @@ const register = async (firstName, lastName, gender, username, password) => {
     return employee;
 }
 
-module.exports = { register }
+const login = async (username, password) => {
+    const employee = await employeeModel.findOne({ username: username });
+    if (!employee) { // no employee found for the provided username
+        throw new InvalidArgsError('Credentials are invalid');
+    } else {
+        const isMatch = await bcrypt.compare(password, employee.password);
+        if (isMatch) { // valid employee
+            const token = jsonwebtoken.sign(
+                { jwtid: uuidv4(), username: employee.username, roles: employee.roles }, 
+                process.env.JWT_SECRET,
+                { algorithm: 'HS512', expiresIn: '1d' }
+            );
+            return token;
+        } else { // password does not match
+            throw new InvalidArgsError('Credentials are invalid');
+        }
+    }
+}
+
+module.exports = { register, login }
