1f85464e06531e11c67a4be7c62d705958fe725e
lalankeba <lalankeba@gmail.com>
2024-05-18 20:23:39 +0530

    adding test cases for login

--- tests/auth-service.test.js
+++ tests/auth-service.test.js
@@ -1,12 +1,14 @@
+require('dotenv').config();
 const employeeModel = require('../models/employee-model');
 const { register, login } = require('../services/auth-service');
 const mongoose = require('mongoose');
 const InvalidArgsError = require('../errors/invalid-args-error');
-const bcrypt = require('bcryptjs');
+const bcryptjs = require('bcryptjs');
 
 jest.mock('../models/employee-model');
 jest.mock('bcryptjs', () => ({
-    hash: jest.fn().mockResolvedValue('hashed$2a$10$WMTof/EfqMIoT2foX0N9x3NZL.XnVPn8g6value')
+    hash: jest.fn().mockResolvedValue('hashed$2a$10$WMTof/EfqMIoT2foX0N9x3NZL.XnVPn8g6value'),
+    compare: jest.fn().mockResolvedValue(true)
 }));
 
 describe('auth', () => {
@@ -31,7 +33,7 @@ describe('auth', () => {
             lastName,
             gender,
             username,
-            password: await bcrypt.hash(password, 10),
+            password: await bcryptjs.hash(password, 10),
             roles,
             toObject: function() {
                 const { password, ...rest } = this;
@@ -68,7 +70,7 @@ describe('auth', () => {
             lastName,
             gender,
             username,
-            password: await bcrypt.hash(password, 10),
+            password: await bcryptjs.hash(password, 10),
             roles,
             toObject: function() {
                 const { password, ...rest } = this;
@@ -81,5 +83,85 @@ describe('auth', () => {
         // Act & Assert
         await expect(register(firstName, lastName, gender, username, password))
             .rejects.toThrow(InvalidArgsError);
+    });
+
+    it('Should login an employee', async () => {
+        // Arrange
+        const id = new mongoose.Types.ObjectId();
+        const firstName = 'John';
+        const lastName = 'Doe';
+        const gender = "MALE";
+        const username = "john@example.com";
+        const password = "Abcd@1234";
+        const roles = ["EMPLOYEE"];
+        
+        const employee = {
+            _id: id,
+            firstName,
+            lastName,
+            gender,
+            username,
+            password: await bcryptjs.hash(password, 10),
+            roles,
+            toObject: function() {
+                const { password, ...rest } = this;
+                return rest;
+            }
+        };
+
+        employeeModel.findOne.mockResolvedValue(employee);
+        bcryptjs.compare.mockResolvedValue(true);
+
+        // Act
+        const token = await login(username, password);
+
+        // Assert
+        expect(token).toBeTruthy();
+    });
+
+    it('Should not login a non existent employee', async () => {
+        // Arrange
+        const username = "john@example.com";
+        const password = "Abcd@1234";
+
+        employeeModel.findOne.mockResolvedValue(undefined);
+
+        // Act & Assert
+        await expect(login(username, password))
+            .rejects.toThrow(InvalidArgsError);
+
+    });
+
+    it('Should not login an employee when passwords do not match', async () => {
+        // Arrange
+        const id = new mongoose.Types.ObjectId();
+        const firstName = 'John';
+        const lastName = 'Doe';
+        const gender = "MALE";
+        const username = "john@example.com";
+        const password = "Abcd@1234";
+        const roles = ["EMPLOYEE"];
+        
+        const employee = {
+            _id: id,
+            firstName,
+            lastName,
+            gender,
+            username,
+            password: await bcryptjs.hash(password, 10),
+            roles,
+            toObject: function() {
+                const { password, ...rest } = this;
+                return rest;
+            }
+        };
+
+        employeeModel.findOne.mockResolvedValue(employee);
+        bcryptjs.compare.mockResolvedValue(false);
+
+        // Act & Assert
+        await expect(login(username, password))
+            .rejects.toThrow(InvalidArgsError);
+
     });
 });
